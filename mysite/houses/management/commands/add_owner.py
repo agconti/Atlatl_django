e3a7ee45108bb1ae7130c20e838ec36ce581a566
agconti <agc11d@gmail.com>
2013-07-19 16:51:28 -0500

    Updated add owner custom function, included test of function

--- mysite/houses/management/commands/add_owner.py
+++ mysite/houses/management/commands/add_owner.py
@@ -1,15 +1,17 @@
+from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 from houses.models import House, Owner
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--name',
-            action='store_true',
+            action='store',
+            type = 'string',
             dest='name',
             default=False,
             help='Add Owner to owner Model'),
         )
-    
+
     def handle(self, *args, **options):
         if options['name']:
             try:
@@ -18,4 +20,4 @@ class Command(BaseCommand):
             except Owner.unique_error_message:
                 raise CommandError('Owner "%s" already exists' % options['name'])
     
-            self.stdout.write('Successfully added Owner "%s"' % options['name'])
+            self.stdout.write('Successfully Added Owner Name="%s"' % options['name'])
