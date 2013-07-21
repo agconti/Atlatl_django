49fd97a8c2ddd069eca087c9930b83220fca3529
agconti <agc11d@gmail.com>
2013-07-21 15:14:03 -0500

    added webpage to shouw all houses and owners view

--- mysite/houses/views.py
+++ mysite/houses/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from houses.models import House, Owner
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the houses index.")
+    house_list = House.objects.all().order_by('owner')
+    context = {'house_list': house_list}
+    return render(request, 'houses/index.html', context)
